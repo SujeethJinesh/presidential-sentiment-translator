262724930f49714724f6bd0a8c833872fafccd09
SujeethJinesh <sujeethjinesh@gmail.com>
2020-02-20 21:52:46 -0800

    Add Heroku

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux'; 
+import Root from './components/Root';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(A)
+
+// ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
